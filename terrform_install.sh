108f6321eefc5477913e2d018936fd2e9c1d0ceb
root <root@ip-172-31-45-48.us-east-2.compute.internal>
2020-08-05 08:26:53 +0000

    Terraform install script

---
+++ terrform_install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+cd ~
+sudo wget https://releases.hashicorp.com/terraform/0.12.24/terraform_0.12.24_linux_amd64.zip
+sudo unzip terraform_0.12.24_linux_amd64.zip
+sudo mv terraform /usr/bin/
